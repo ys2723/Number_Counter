d301889e005c2d696a4070478b32bd3b9f906c5b
Yash Tripathi <yashtripathi2205@gmail.com>
2023-06-10 10:50:46 +0530

    Create script.js
    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let incrementNumber = document.getElementById("count-display") // for connecting icnrement button
+let decrementNumber = document.getElementById("count-display") // for connecting decrement button
+let saveNumber = document.getElementById("save-element") // for connecting the save number button
+let backtozeroNumber = document.getElementById("count-display") // for going back to zero
+
+let count = 0 
+console.log(incrementNumber)
+
+// below function increments the count
+function increment(){
+   count = count + 1
+   incrementNumber.innerText = count
+}
+
+// below function decrements the count
+function decrement(){
+    count = count - 1
+    decrementNumber.innerText = count
+}
+
+// below function saves the count
+function save(){
+    let countStr = count + " - "
+    saveNumber.textContent += countStr
+}
+console.log(count) // prints the saved count
+
+// below function gets the count back to zero
+function backtozero(){
+    backtozeroNumber.textContent = 0
+    count = 0
+}
